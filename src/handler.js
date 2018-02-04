f8e42167b1e053cf1ecea3444929b18584a18a1c
Michele Giardi <miki79@users.noreply.github.com>
2018-02-04 00:38:56 +0000

    refactor handler

--- src/handler.js
+++ src/handler.js
@@ -4,35 +4,39 @@ const { getNotification } = require('./justwatch');
 const notificationEmail = process.env.NOTIFICATION_EMAIL;
 const token = process.env.TOKEN_JUSTWATCH;
 
+const sendEmail = (notifications, callback) => {
+  const params = {
+    Destination: {
+      ToAddresses: [notificationEmail],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: JSON.stringify(notifications),
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'JustWatch notification',
+      },
+    },
+    Source: notificationEmail,
+  };
+  const ses = new AWS.SES({ region: process.env.AWS_REGION });
+  ses.sendEmail(params, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, true);
+    }
+  });
+};
+
 module.exports.newRelease = (event, context, callback) => {
   getNotification(token)
     .then((notifications) => {
       if (notifications && notifications.length > 0) {
-        const params = {
-          Destination: {
-            ToAddresses: [notificationEmail],
-          },
-          Message: {
-            Body: {
-              Text: {
-                Data: JSON.stringify(notifications),
-              },
-            },
-            Subject: {
-              Charset: 'UTF-8',
-              Data: 'JustWatch notification',
-            },
-          },
-          Source: notificationEmail,
-        };
-        const ses = new AWS.SES({ region: process.env.AWS_REGION });
-        ses.sendEmail(params, (err) => {
-          if (err) {
-            callback(err);
-          } else {
-            callback(null, true);
-          }
-        });
+        sendEmail(notifications, callback);
       } else {
         callback(null, false);
       }
